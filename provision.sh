133e35e5fcda963e763272722e4d8d0b5ed8d3eb
matthew-shaw <matthew.shaw@hotmail.co.uk>
2014-07-08 15:35:30 +0100

    added tests file

--- provision.sh
+++ provision.sh
@@ -11,6 +11,12 @@ echo Installing PIP...
 sudo apt-get install -y python-pip
 
 echo Installing Flask...
-sudo pip install flask
+sudo pip install flask==0.10.1
+
+echo Installing Jinja...
+sudo pip install jinja2==2.7.2
+
+echo Installing GUnicorn
+sudo pip install gunicorn==18.0
 
 echo 'cd /vagrant' >> /home/vagrant/.bash_profile
