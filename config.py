e7306063de324e742596897ffa22968246658794
Adam Shimali <adam.shimali@gmail.com>
2014-07-13 18:38:14 +0100

    Fix markup for view property page

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 class Config(object):
     DEBUG=False
     TITLE_API_URL = os.environ.get('TITLE_API_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class DevelopmentConfig(Config):
     DEBUG=True
