86702345b1b8063f802fc0fed3e03e559851a5d2
Mat Wall <mat.wall@digital.cabinet-office.gov.uk>
2014-07-14 11:58:39 +0100

    Configured ports correctly.
    Now gets HTTP port from dev environment
    Configured port for public-titles-api

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 export SETTINGS='config.DevelopmentConfig'
-export TITLE_API_URL='http://localhost:8003'
-export PORT='8002'
+export TITLE_API_URL='http://localhost:8004'
 
 python run_dev.py
