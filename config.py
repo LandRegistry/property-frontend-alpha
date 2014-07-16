b51a33f9048fe2dea07765f0c5298297f69e5d99
Adam Shimali <adam.shimali@gmail.com>
2014-07-16 13:26:44 +0100

    Clean up config

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 import os
 
 class Config(object):
-    DEBUG=False
+    DEBUG = False
     TITLE_API_URL = os.environ.get('TITLE_API_URL')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class DevelopmentConfig(Config):
-    DEBUG=True
+    DEBUG = True
+
+class TestConfig(DevelopmentConfig):
+    TESTING = True
