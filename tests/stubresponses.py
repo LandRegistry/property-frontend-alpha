1cb8209d6e58f7ad226809c98a064213228dfa81
Adam Shimali <adam.shimali@gmail.com>
2014-08-01 16:33:40 +0100

    Cleaned up mocking of requests in test_app

---
+++ tests/stubresponses.py
@@ -0,0 +1,18 @@
+title = {
+        "title_number": "TN1234567",
+        "property": {
+            "address" :
+            {
+                "title_number" : "TN1",
+                "house_number" : "12",
+                "road" :"High St",
+                "town" : "Sometown",
+                "postcode" : "ABC 123"
+            }
+        },
+        "payment":{
+            "price_paid" : 1000
+        }
+    }
+
+search_results = {"results" : [title]}
--- tests/test_app.py
+++ tests/test_app.py
@@ -3,40 +3,44 @@ import mock
 import unittest
 import requests
 
-from mockresponses import MockProperty
-from mockresponses import Mock404
-from mockresponses import MockSearchResults
-from mockresponses import Mock500
-
+from stubresponses import title
+from stubresponses import search_results
 
 class ViewPropertyTestCase(unittest.TestCase):
 
     def setUp(self):
-        app.config['TESTING'] = True
         self.search_api = app.config['SEARCH_API']
         self.service_api = app.config['SERVICE_FRONTEND_URL']
         self.app = app.test_client()
 
-    @mock.patch('requests.get', return_value=MockProperty())
+    @mock.patch('requests.get')
     def test_get_property_calls_search_api(self, mock_get):
         title_number = "TN1234567"
         self.app.get('/property/%s' % title_number)
         mock_get.assert_called_with('%s/titles/%s' % (self.search_api, title_number))
 
-    @mock.patch('requests.get', return_value=Mock404())
-    def test_get_property_preserves_downstream_404(self, mock_get):
+    @mock.patch('requests.get')
+    @mock.patch('requests.Response')
+    def test_get_property_preserves_downstream_404(self, mock_response, mock_get):
+        mock_response.status_code = 404
+        mock_response.raise_for_status.side_effect = requests.exceptions.HTTPError
+        mock_get.return_value = mock_response
         title_number = "TN1234567"
         response = self.app.get('/property/%s' % title_number)
         assert response.status_code == 404
 
-    @mock.patch('requests.get', return_value=MockSearchResults())
+    @mock.patch('requests.get', returns=search_results)
     def test_search_results_calls_search_api(self, mock_get):
         search_query = "TN12"
         self.app.post('/search/results', data=dict(search=search_query))
         mock_get.assert_called_with('%s/search?query=%s' % (self.search_api, search_query))
 
-    @mock.patch('requests.get', return_value=Mock500())
-    def test_get_property_preserves_downstream_500(self, mock_get):
+    @mock.patch('requests.get')
+    @mock.patch('requests.Response')
+    def test_get_property_preserves_downstream_500(self, mock_response, mock_get):
+        mock_response.status_code = 500
+        mock_response.raise_for_status.side_effect = requests.exceptions.HTTPError
+        mock_get.return_value = mock_response
         search_query = "TN12"
         response = self.app.post('/search/results', data=dict(search=search_query))
         assert response.status_code == 500
@@ -47,8 +51,12 @@ class ViewPropertyTestCase(unittest.TestCase):
         response = self.app.post('/search/results', data=dict(search=search_query))
         assert response.status_code == 500
 
-    @mock.patch('requests.get', return_value=MockProperty())
-    def test_for_service_frontend_link(self, mock_get):
+    @mock.patch('requests.get')
+    @mock.patch('requests.Response')
+    def test_for_service_frontend_link(self, mock_response, mock_get):
+      mock_response.status_code = 200
+      mock_response.json.return_value = title
+      mock_get.return_value = mock_response
       title_number = "TN1234567"
       rv = self.app.get('/property/%s' % title_number)
       service_frontend_url = '%s/%s/%s' % (self.service_api, 'property', title_number)
@@ -56,7 +64,6 @@ class ViewPropertyTestCase(unittest.TestCase):
 
     def test_currency_format(self):
         from propertyfrontend.server import currency
-
         assert '1.00' == currency(1)
         assert '10.00' == currency(10)
         assert '100.00' == currency(100)
