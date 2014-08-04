b672b8aec1fc3a5adb9fafddba1b2066da77debe
matthew-shaw <matthew.shaw@hotmail.co.uk>
2014-08-04 18:27:52 +0100

    Using OS OpenSpace in OSGB36

--- propertyfrontend/templates/js/map.js
+++ propertyfrontend/templates/js/map.js
@@ -1,10 +1,27 @@
-var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-	osmAttrib = '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	osm = L.tileLayer(osmUrl, {maxZoom: 18, attribution: osmAttrib});
+var osgb = new L.Proj.CRS('EPSG:27700',
+  '+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000' +
+  '+ellps=airy +datum=OSGB36 +units=m +no_defs',
+  {
+    resolutions: [2500, 1000, 500, 200, 100, 50, 25, 10, 5, 2.5],
+    bounds: L.bounds([1300000,0],[700000,0])
+  }
+);
 
-var map = L.map('map').setView([51.505, -0.159], 15).addLayer(osm);
+/* L.Map with OS options */
+var map = new L.Map('map', {
+  crs: osgb,
+  continuousWorld: true,
+  worldCopyJump: false,
+  minZoom: 2,
+  maxZoom: L.OSOpenSpace.RESOLUTIONS.length - 1,
+});
 
-L.marker([51.504, -0.159])
-	.addTo(map)
-	.bindPopup('A pretty CSS3 popup.<br />Easily customizable.')
-	.openPopup();
+/* New L.TileLayer.OSOpenSpace with API Key */
+openspaceLayer = L.tileLayer.osopenspace("FFB702322FDF714DE0430B6CA40A06C6", {debug: true});
+
+map.addLayer(openspaceLayer);
+map.setView([51.504, -0.159], 9);
+
+/* add some ui elems to the map */
+
+L.control.scale().addTo(map);
