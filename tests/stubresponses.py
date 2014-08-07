567f4586da0cc778b2a3a995a8f612ec978951e9
Juan Matthys Uys <opyate@gmail.com>
2014-08-07 17:21:27 +0100

    fix tests

--- tests/stubresponses.py
+++ tests/stubresponses.py
@@ -1,18 +1,58 @@
 title = {
-        "title_number": "TN1234567",
-        "property": {
-            "address" :
-            {
-                "title_number" : "TN1",
-                "house_number" : "12",
-                "road" :"High St",
-                "town" : "Sometown",
-                "postcode" : "ABC 123"
-            }
-        },
-        "payment":{
-            "price_paid" : 1000
+    "title_number": "TN1234567",
+    "property": {
+      "address" :
+      {
+        "title_number" : "TN1",
+        "house_number" : "12",
+        "road" :"High St",
+        "town" : "Sometown",
+        "postcode" : "ABC 123"
         }
+      },
+    "payment":{
+      "price_paid" : 1000
+      },
+    "extent": {
+      "crs": {
+        "properties": {
+          "name": "urn:ogc:def:crs:EPSG:27700"
+          }, 
+        "type": "name"
+        }, 
+      "geometry": {
+        "coordinates": [
+          [
+            [
+              [
+                530857.01, 
+                181500.0
+                ], 
+              [
+                530857.0, 
+                181500.0
+                ], 
+              [
+                530857.0, 
+                181500.0
+                ], 
+              [
+                530857.0, 
+                181500.0
+                ], 
+              [
+                530857.01, 
+                181500.0
+                ]
+              ]
+            ]
+          ], 
+        "type": "MultiPolygon"
+        }, 
+      "properties": {}, 
+      "type": "Feature"
+      } 
+
     }
 
 search_results = {"results" : [title]}
