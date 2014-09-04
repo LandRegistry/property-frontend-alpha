2cedf83bf66ff281e06472e7e6693763b1381811
Mat Wall <mat.wall@digital.cabinet-office.gov.uk>
2014-09-04 16:10:47 +0000

    Configured for new dev environemnt

--- environment.sh
+++ environment.sh
@@ -1,3 +1,2 @@
-export SETTINGS='config.DevelopmentConfig'
 export SEARCH_API='http://localhost:8003'
 export SERVICE_FRONTEND_URL='http://service-frontend.landregistry.local'
--- environment_test.sh
+++ environment_test.sh
@@ -1,3 +1,2 @@
-export SETTINGS='config.TestConfig'
 export SEARCH_API='http://localhost:8003'
 export SERVICE_FRONTEND_URL='http://localhost:8007'
